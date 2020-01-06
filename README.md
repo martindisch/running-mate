49db5d5b9c65042c5a7024d5d860718f8b868039
Martin Disch <martindisch@gmail.com>
2020-01-06 15:20:34 +0100

    Implement sample echo bot with webhook

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 A Telegram chatbot that helps you pick up running.
 
+## Getting started
+1. Install Docker & Docker Compose
+2. Create and edit `.env`. For reference, see `.env.TEMPLATE`.
+
+## Run
+To get everything up and running, use `docker-compose up`. If you change some
+source files, do `docker-compose up --build` to rebuild the image with your
+changes. To remove all traces of it from your system, do `docker-compose down
+-v --rmi all`.
+
+## TLS
+The whole application is TLS-agnostic to make containerized deployments easy.
+If you want to use TLS, simply set up a reverse proxy like nginx to handle it.
+See the files in `nginx` for an example.
+
 ## License
 Licensed under either of
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde_json::{json, Value};
+use std::env;
 
-async fn index(info: web::Path<(String, u32)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.0, info.1)
+async fn handle(update: web::Json<Value>) -> HttpResponse {
+    // Get untyped (fallible) information from message
+    let chat_id = &update["message"]["chat"]["id"];
+    let user = &update["message"]["from"]["first_name"];
+    let text = &update["message"]["text"];
+
+    // Respond using the same HTTP request
+    let response =
+        format!("Hello, {}! You sent:\n{}", user.as_str().unwrap(), text);
+    HttpResponse::Ok().json(
+        json!({"method": "sendMessage", "chat_id": chat_id, "text": response}),
+    )
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(web::resource("/{name}/{id}/index.html").to(index))
+    // The endpoint we're listening on is a secret
+    let endpoint = env::var("TELEGRAM_WEBHOOK").unwrap();
+
+    // Start server listening for webhook requests
+    HttpServer::new(move || {
+        App::new().route(&endpoint, web::post().to(handle))
     })
     .bind("0.0.0.0:8080")?
     .run()
